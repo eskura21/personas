ad2b1aaaf069421d8ff1e34dff89fe91da7b1ade
Javier Lete <javier@eskura21.com>
2019-10-11 18:52:59 +0200

    Rutado listado

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { PersonasTablaComponent } from './personas-tabla/personas-tabla.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/listado', pathMatch: 'full' },
+  { path: 'listado', component: PersonasTablaComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
