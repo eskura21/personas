1c3288599b7d457b4b0fe8f82a460c4d8e89d3cc
Javier Lete <javier@eskura21.com>
2019-10-11 17:47:55 +0200

    Conectamos desde el servicio al servidor REST

--- src/app/persona.service.ts
+++ src/app/persona.service.ts
@@ -1,17 +1,19 @@
 import { Injectable } from '@angular/core';
 import { Persona } from './persona';
 import { Observable, of } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PersonaService {
 
+  private url = 'http://localhost:3000/personas/';
   personas: Persona[];
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
   getPersonas(): Observable<Persona[]> {
-    return of(this.personas);
+    return this.http.get<Persona[]>(this.url);
   }
 }
