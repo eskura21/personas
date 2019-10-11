0db26da9c8bbfb1d3088466db6f69d58042a4b51
Javier Lete <javier@eskura21.com>
2019-10-11 17:25:41 +0200

    Creamos colección en el componente

--- src/app/personas-tabla/personas-tabla.component.ts
+++ src/app/personas-tabla/personas-tabla.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Persona } from '../persona';
 
 @Component({
   selector: 'app-personas-tabla',
@@ -6,6 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./personas-tabla.component.css']
 })
 export class PersonasTablaComponent implements OnInit {
+
+  personas: Persona[] = [
+    { id: 1, nombre: 'Javier', email: 'javier@eskura21.com' },
+    { id: 2, nombre: 'Pepe', email: 'pepe@sucasa.com' }
+  ];
 
   constructor() { }
 
