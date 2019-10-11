d2fd6578a2663511893774bbac9828162423e06b
Javier Lete <javier@eskura21.com>
2019-10-11 18:58:48 +0200

    Rutados a detalles

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { PersonasTablaComponent } from './personas-tabla/personas-tabla.component';
+import { PersonaDetallesComponent } from './persona-detalles/persona-detalles.component';
 
 
 const routes: Routes = [
-  { path: '', redirectTo: '/listado', pathMatch: 'full' },
-  { path: 'listado', component: PersonasTablaComponent },
+  { path: '', redirectTo: '/personas-listado', pathMatch: 'full' },
+  { path: 'personas-listado', component: PersonasTablaComponent },
+  { path: 'persona-detalles', component: PersonaDetallesComponent }
 ];
 
 @NgModule({
