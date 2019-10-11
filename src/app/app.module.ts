30ecbc3446913ce5cf0a60b99bf7e6a32e4746fa
Javier Lete <javier@eskura21.com>
2019-10-11 17:47:27 +0200

    Importamos el módulo de HTTP

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,8 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PersonasTablaComponent } from './personas-tabla/personas-tabla.component';
 
+import { HttpClientModule } from '@angular/common/http';
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -12,7 +14,8 @@ import { PersonasTablaComponent } from './personas-tabla/personas-tabla.componen
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
