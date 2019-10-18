94db7ef3e6e11f86ad74d405299e22922412307b
Javier Lete <javier@eskura21.com>
2019-10-18 16:29:27 +0200

    Extracción del id de detalles

--- src/app/persona-detalles/persona-detalles.component.ts
+++ src/app/persona-detalles/persona-detalles.component.ts
@@ -1,17 +1,25 @@
 import { Component, OnInit } from '@angular/core';
 import { Persona } from '../persona';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-persona-detalles',
   templateUrl: './persona-detalles.component.html',
   styleUrls: ['./persona-detalles.component.css']
 })
-export class PersonaDetallesComponent {
+export class PersonaDetallesComponent implements OnInit {
   persona: Persona = {
     id: 100,
     nombre: 'Prueba',
     email: 'suemail@email.com'
   };
+
+  constructor(private route: ActivatedRoute) { }
+
+  ngOnInit(): void {
+    const id: number = +this.route.snapshot.paramMap.get('id');
+    console.log(id);
+  }
 
   onAceptar() {
     console.log(this.persona);
