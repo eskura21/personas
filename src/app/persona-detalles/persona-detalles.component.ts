7f9082cdaf6bbc025af5f8b5a2b745aea370e836
Javier Lete <javier@eskura21.com>
2019-10-18 16:51:47 +0200

    Servicio getPersona y botón modificar funciona

--- src/app/persona-detalles/persona-detalles.component.ts
+++ src/app/persona-detalles/persona-detalles.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Persona } from '../persona';
 import { ActivatedRoute } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+import { PersonaService } from '../persona.service';
 
 @Component({
   selector: 'app-persona-detalles',
@@ -14,11 +16,16 @@ export class PersonaDetallesComponent implements OnInit {
     email: 'suemail@email.com'
   };
 
-  constructor(private route: ActivatedRoute) { }
+  constructor(
+    private route: ActivatedRoute,
+    private personaService: PersonaService) { }
 
   ngOnInit(): void {
     const id: number = +this.route.snapshot.paramMap.get('id');
     console.log(id);
+    this.personaService.getPersona(id).subscribe(
+      personaRecibida => this.persona = personaRecibida
+    );
   }
 
   onAceptar() {
--- src/app/persona.service.ts
+++ src/app/persona.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Persona } from './persona';
-import { Observable } from 'rxjs';
+import { Observable, of } from 'rxjs';
 import { catchError } from 'rxjs/operators';
 import { HttpClient } from '@angular/common/http';
 import { MensajeService } from './mensaje.service';
@@ -19,13 +19,27 @@ export class PersonaService {
 
   getPersonas(): Observable<Persona[]> {
     return this.http.get<Persona[]>(this.url).pipe(
+      catchError(this.gestionarError<Persona[]>('getPersonas', []))
+    );
+  }
+
+  getPersona(id: number): Observable<Persona> {
+    return this.http.get<Persona>(this.url + id).pipe(
       catchError(
-        (error) => {
-          console.error('PersonaService.getPersonas()', error.message);
-          this.mensajeService.setMensaje('Error al obtener las personas', 'danger');
-          return [];
-        }
+        this.gestionarError<Persona>(
+          `getPersona(${id})`,
+          { id: 0, nombre: '', email: '' }
+        )
       )
     );
+  }
+
+  private gestionarError<T>(categoria: string, objetoADevolver: T):
+    (err: any, caught: Observable<T>) => Observable<T> {
+    return (error) => {
+      console.error(categoria, error.message);
+      this.mensajeService.setMensaje('Error al obtener las personas', 'danger');
+      return of(objetoADevolver);
+    };
   }
 }
