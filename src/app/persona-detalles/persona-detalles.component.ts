a62c8efd82b570a09c13418c9186006dbb967d31
Javier Lete <javier@eskura21.com>
2019-10-18 17:33:52 +0200

    Botón añadir pasa a pantalla limpia

--- src/app/persona-detalles/persona-detalles.component.ts
+++ src/app/persona-detalles/persona-detalles.component.ts
@@ -11,9 +11,9 @@ import { PersonaService } from '../persona.service';
 })
 export class PersonaDetallesComponent implements OnInit {
   persona: Persona = {
-    id: 100,
-    nombre: 'Prueba',
-    email: 'suemail@email.com'
+    id: 0,
+    nombre: '',
+    email: ''
   };
 
   constructor(
@@ -23,12 +23,21 @@ export class PersonaDetallesComponent implements OnInit {
   ngOnInit(): void {
     const id: number = +this.route.snapshot.paramMap.get('id');
     console.log(id);
-    this.personaService.getPersona(id).subscribe(
-      personaRecibida => this.persona = personaRecibida
-    );
+
+    if (id !== 0) {
+      this.personaService.getPersona(id).subscribe(
+        personaRecibida => this.persona = personaRecibida
+      );
+    }
   }
 
   onAceptar() {
     console.log(this.persona);
+
+    if (this.persona.id === 0) {
+      console.log('Insertar');
+    } else {
+      console.log('Modificación');
+    }
   }
 }
